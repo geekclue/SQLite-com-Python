9625f8b8d956ca4d03f8102008e0c9beb7bc9434
geekclue <geekclue@gmail.com>
2021-05-07 12:24:37 -0300

    Versão 2

--- Aula01/conectaDB.py
+++ Aula01/conectaDB.py
@@ -1,15 +1,18 @@
 #File: conectaDB.py
-
+import os
 import sqlite3
-from sqlite3 import Error
 
 def conectar(db_path):
     conexao = None
     try:
-        conexao = sqlite3.connect(db_path)
-    except Error as err:
+        if not os.path.exists(db_path):
+            conexao = sqlite3.connect(db_path, timeout=20)
+
+    except sqlite3.DatabaseError as err:
         print(f"Erro ao conectar o banco de dados {db_path}.")
+    
     return conexao
+
 
 def desconectar(conexao = None):
     if conexao is not None:
